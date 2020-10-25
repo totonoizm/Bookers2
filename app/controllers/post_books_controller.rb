6fb87817b757cd0e9695296fc232353ac7160559
EC2 Default User <ec2-user@ip-172-31-44-141.ec2.internal>
2020-10-25 15:12:23 +0000

    [add]投稿機能コントローラ記述

--- app/controllers/post_books_controller.rb
+++ app/controllers/post_books_controller.rb
@@ -6,11 +6,21 @@ class PostBooksController < ApplicationController
   end
 
   def new
+    @post_book = PostBook.new
   end
 
   def create
+    @post_book = PostBook.new(post_book_params)
+    @post_book.user_id = current_user.id
+    @post_book.saved
+    redirect_to post_book_path
   end
 
   def show
+  end
+
+  praivate
+  def post_book_params
+    params.require(:post_book).permit(:title, :body)
   end
 end
