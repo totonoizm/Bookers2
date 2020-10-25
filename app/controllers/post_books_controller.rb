9cf08b4ec5232391dd57961adfdd3a6b6dea5cd3
EC2 Default User <ec2-user@ip-172-31-44-141.ec2.internal>
2020-10-25 13:47:45 +0000

    [add]コントローラ作成

---
+++ app/controllers/post_books_controller.rb
@@ -0,0 +1,16 @@
+class PostBooksController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :post_books, only: [:new, :create, :index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
