6d700062fe4d7806329a4b629067de0c8665b01a
EC2 Default User <ec2-user@ip-172-31-44-141.ec2.internal>
2020-10-25 13:06:39 +0000

    error

--- db/migrate/20201025122759_create_books.rb
+++ db/migrate/20201025122759_create_books.rb
@@ -3,7 +3,7 @@ class CreateBooks < ActiveRecord::Migration[5.2]
     create_table :books do |t|
       t.text :title
       t.text :body
-      t.user_id :
+      t.user_id :integer
 
       t.timestamps
     end
