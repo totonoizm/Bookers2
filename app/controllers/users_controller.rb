7bd50116b96cf30ccfdcce5418e791f1a3cf459e
EC2 Default User <ec2-user@ip-172-31-44-141.ec2.internal>
2020-10-26 06:37:59 +0000

    mypage作成中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @post_books = @user.post_books.page(params[:page]).reverse_order
   end
 
   def edit
