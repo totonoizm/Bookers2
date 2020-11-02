95467ed42f5a711630430d2a0affa3b47681d3a9
EC2 Default User <ec2-user@ip-172-31-44-141.ec2.internal>
2020-11-02 06:54:10 +0000

    [fix]editerror

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,7 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
-    if @book_user == current_user
+    if @book.user == current_user
       render 'edit'
     else
       redirect_to books_path
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,11 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+      render 'edit'
+    else
+      redirect_to books_path
+    end
   end
 
   def update
